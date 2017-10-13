3aa11b982c8705d3bcb27d12599e99fc9c764fbc
Ruben G. Eide <ruereid@gmail.com>
2017-10-13 14:09:43 +0200

    server fix 0.0.1

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const app = express()
 
+
+//app.get('/', function (req, res) {
+//  res.send('Hello World!')
+//})
+
+app.use(express.static(__dirname + '/public'));
+
+function randomGreeting(){
+    
+    var Array = ["hei", "hallo", "hola"];
+
+    toBeShown = Array[Math.floor(Math.random() * 3) ];
+
+    return toBeShown;
+}
+
+app.get('/hehe/:navn', function (req, res) {
+  res.send(randomGreeting() + " " + req.params.navn)
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
+
+//app.use(express.static('public'))
+
+
+
+
+
+/*
+const express = require('express')
+const app = express()
+
 app.use(express.static(__dirname + '/public'));
 //app.use(express.static('./public'))
 
@@ -10,4 +44,4 @@ app.get('/:id', function (req, res) {
 
 app.listen(8080, function() {
   console.log('localhost:8080')
-})
+})*/
